045697262658b8815485b03752f3820273b97c4d
luizgbbosco <luizgbbosco@gmail.com>
2018-08-29 11:09:50 -0300

    adicionando funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AlertModule} from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule,
-    CursosModule
+    CursosModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
